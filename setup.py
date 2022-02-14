be3c6b4fd8221dc9de476d281196f7b3bd61b40d
Laurens Voet <lvoet@mit.edu>
2022-02-14 10:11:26 -0500

    Update setup file

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
 	name='pyNA',
 	author="Laurens Jozef A. Voet",
-	author_email="laurens.ja.voet@gmail.com",
-	description="python Noise Assessment tool to assess aircraft noise during take-off and landing operations.",
+	author_email="lvoet@mit.edu",
+	description="pyNA (python Noise Assessment) computes aircraft noise on take-off trajectories.",
 	version='1.0.0',
 	packages=['pyNA'],
-	url="https://github.com/MIT-LAE/pyNA",
+	url="https://github.mit.edu/lvoet/pyNA",
 	include_package_data=True,
 	package_data={'': ['data/*']},
 	install_requires=[
@@ -21,7 +21,8 @@ setup(
 		'openmdao>=3.16.0',
 		'pandas==1.0.5',
 		'scipy>1.7.0',
-		'tqdm==4.47.0'
+		'tqdm==4.47.0',
+		'openpyxl>=3.0.5'
 	],
 	python_requires=">=3.7"
 )
